a5d8abad651f28936101986af179a51630dc8aae
lxcao <caolingxin@hotmail.com>
2021-06-15 17:59:01 +0800

    http repository

---
+++ lib/failure_model.dart
@@ -0,0 +1,12 @@
+/*
+ * @Author: clingxin
+ * @Date: 2021-06-15 17:47:17
+ * @LastEditors: clingxin
+ * @LastEditTime: 2021-06-15 17:47:26
+ * @FilePath: /flutter_notion/lib/failure_model.dart
+ */
+class Failure {
+  final String message;
+
+  const Failure({required this.message});
+}
--- lib/item_model.dart
+++ lib/item_model.dart
@@ -2,7 +2,7 @@
  * @Author: clingxin
  * @Date: 2021-06-15 16:53:54
  * @LastEditors: clingxin
- * @LastEditTime: 2021-06-15 17:24:19
+ * @LastEditTime: 2021-06-15 17:50:30
  * @FilePath: /flutter_notion/lib/item_model.dart
  */
 
@@ -18,4 +18,16 @@ class Item {
     required this.price,
     required this.date,
   });
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    final properties = map['properties'] as Map<String, dynamic>;
+    final nameList = (properties['Name']?['title'] ?? []) as List;
+    final dateStr = properties['Date']?['date']?['start'];
+    return Item(
+      name: nameList.isNotEmpty ? nameList[0]['plain_text'] : '?',
+      category: properties['Category']?['select']?['name'] ?? 'Any',
+      price: (properties['Price']?['number'] ?? 0).toDouble(),
+      date: dateStr != null ? DateTime.parse(dateStr) : DateTime.now(),
+    );
+  }
 }
